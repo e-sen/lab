a5a1b3e8aad77f416490746595e4befb600309c6
e-sen <i@iamee.me>
2019-02-17 16:02:24 +0800

    refactor(change module name):

--- common/src/main/kotlin/esen/command/CreateUserCommand.kt
+++
@@ -1,7 +0,0 @@
-package esen.command
-
-/**
- * CreateUserCommand
- * @author Iamee
- * @create 2019-02-16 21:02
- */
--- common/src/main/kotlin/esen/event/UserCreatedEvent.kt
+++
@@ -1,7 +0,0 @@
-package esen.event
-
-/**
- * UserCreatedEvent
- * @author Iamee
- * @create 2019-02-16 21:05
- */
